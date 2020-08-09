f505d38b7908ad6b58b93539d9ab5c912ccc8935
Gustavo Alves <gustavo.alves@vtex.com.br>
2020-08-09 02:30:31 -0300

    Remove WSL

--- windows/setup.ps1
+++ windows/setup.ps1
@@ -1,6 +1,8 @@
 # Install Chocolatey, more about that in: https://chocolatey.org/install
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 # Install day to day softwares
-choco install firefox 7zip vlc qbittorrent spotify discord sumatrapdf veracrypt 
+choco install -y firefox 7zip vlc qbittorrent spotify discord sumatrapdf veracrypt steam
 # Install development related softwares
-choco install wsl2 wsl-ubuntu-1804 microsoft-windows-terminal vscode
+choco install -y microsoft-windows-terminal vscode
+# Unfortunately we need to restart to install WSL 
+Restart-Computers
