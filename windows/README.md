5af9c22309f4324beb722e95b9f7e7f0e25027ef
Gustavo Alves <gugamalves@gmail.com>
2020-08-09 05:56:46 -0700

    Enhance setup.ps1

--- windows/README.md
+++ windows/README.md
@@ -12,3 +12,11 @@ Set-ExecutionPolicy RemoteSigned
 Then run `setup.ps1` and restart your computer when it finishes.
 
 Next step is to install `WSL2` and the lastest WSL Ubuntu. Reference: https://docs.microsoft.com/en-us/windows/wsl/install-win10
+
+Download this [hosts](https://someonewhocares.org/hosts/zero/hosts) file and use it. TODO automate this in setup script.
+
+korra specific notes
+--------------------
+
+* Install [Logitech G hub](https://www.logitechg.com/en-us/innovation/g-hub.html)
+* Install [Radeon](https://www.amd.com/en/support)
--- windows/setup.ps1
+++ windows/setup.ps1
@@ -1,6 +1,11 @@
 # Install Chocolatey, more about that in: https://chocolatey.org/install
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 # Install day to day softwares
-choco install -y firefox 7zip vlc qbittorrent spotify discord sumatrapdf veracrypt steam
+choco install -y firefox 7zip vlc qbittorrent spotify discord sumatrapdf veracrypt steam bitwarden
 # Install development related softwares
-choco install -y microsoft-windows-terminal vscode
+choco install -y microsoft-windows-terminal vscode gitkraken
+# Install korra (my gaming/personal pc) specific software
+choco install -y icue calibre
+# Remove Desfragment and Optimize Schedule. It's bad for your SSDs
+# Found command at https://superuser.com/questions/1210453/windows-10-disable-automatic-defragmentation
+schtasks /Change /DISABLE /TN "\Microsoft\Windows\Defrag\ScheduledDefrag"
