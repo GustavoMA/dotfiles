d5ba99021ae0b6807cefa355e5d038e75775e00d
Gustavo Alves <gugamalves@gmail.com>
2020-09-02 21:38:55 -0300

    Add Google Drive for Calibre sync

--- windows/setup.ps1
+++ windows/setup.ps1
@@ -5,7 +5,7 @@ choco install -y firefox 7zip vlc qbittorrent spotify discord sumatrapdf veracry
 # Install development related softwares
 choco install -y microsoft-windows-terminal vscode gitkraken
 # Install korra (my gaming/personal pc) specific software
-choco install -y icue calibre
+choco install -y icue calibre google-backup-and-sync
 # Remove Desfragment and Optimize Schedule. It's bad for your SSDs
 # Found command at https://superuser.com/questions/1210453/windows-10-disable-automatic-defragmentation
 schtasks /Change /DISABLE /TN "\Microsoft\Windows\Defrag\ScheduledDefrag"
