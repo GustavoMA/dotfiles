e44dcfa84e7b7a10311e18d269d3825e7bec835e
Gustavo Alves <gugamalves@yahoo.com.br>
2021-02-07 11:41:45 -0300

    Remove git-kraken

--- windows/setup.ps1
+++ windows/setup.ps1
@@ -1,9 +1,11 @@
 # Install Chocolatey, more about that in: https://chocolatey.org/install
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 # Install day to day softwares
-choco install -y firefox 7zip vlc qbittorrent spotify discord sumatrapdf veracrypt bitwarden
+choco install -y firefox 7zip vlc qbittorrent spotify discord sumatrapdf bitwarden
+# TODO use this guy
+# choco install -y veracrypt
 # Install development related softwares
-choco install -y microsoft-windows-terminal vscode gitkraken
+choco install -y microsoft-windows-terminal vscode
 # Install korra (my gaming/personal pc) specific software
 choco install -y icue calibre google-backup-and-sync steam epicgameslauncher
 # Install work specific software
