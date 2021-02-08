1d2c15dd7a495123c5ae2b382ae983c1e6fc3648
Gustavo Alves <gugamalves@yahoo.com.br>
2021-02-07 21:28:18 -0300

    Add zsh and ohmyzsh

--- ubuntu/setup.sh
+++ ubuntu/setup.sh
@@ -2,3 +2,8 @@
 
 # Create dotfiles symlinks
 ln -s $(pwd)/.gitconfig ~/.gitconfig
+
+# Installing zsh and ohmyzsh
+sudo apt -y install zsh
+chsh -s $(which zsh)
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
