f3155b64b059b663bbeaddbaf84a9ba5d9be4ebc
Gustavo Alves <gustavo.alves@vtex.com.br>
2021-02-07 19:16:32 -0600

    Start Ubuntu dotflies (#2)
    * Add .gitconfig
    
    * Add FiraCode
    
    * Add zsh and ohmyzsh
    
    * Add postman
    
    * Add openvpn

--- windows/setup.ps1
+++ windows/setup.ps1
@@ -5,11 +5,11 @@ choco install -y firefox 7zip vlc qbittorrent spotify discord sumatrapdf bitward
 # TODO use this guy
 # choco install -y veracrypt
 # Install development related softwares
-choco install -y microsoft-windows-terminal vscode
+choco install -y microsoft-windows-terminal vscode firacode postman
 # Install korra (my gaming/personal pc) specific software
 choco install -y icue calibre google-backup-and-sync steam epicgameslauncher
 # Install work specific software
-choco install -y zoom
+choco install -y zoom openvpn
 # Remove Desfragment and Optimize Schedule. It's bad for your SSDs
 # Found command at https://superuser.com/questions/1210453/windows-10-disable-automatic-defragmentation
 schtasks /Change /DISABLE /TN "\Microsoft\Windows\Defrag\ScheduledDefrag"
