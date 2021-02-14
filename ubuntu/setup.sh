a04cc97f3664de8639d331879832ee6db897e939
Gustavo Alves <gugamalves@yahoo.com.br>
2021-02-14 16:41:56 -0300

    Fix order to add symlink to .zshrc

--- ubuntu/setup.sh
+++ ubuntu/setup.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-# Create dotfiles symlinks
-ln -s $(pwd)/.gitconfig ~/.gitconfig
-ln -s $(pwd)/.zshrc ~/.zshrc
-
 # Installing zsh and ohmyzsh
 sudo apt -y install zsh
 chsh -s $(which zsh)
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+rm ~/.zshrc
+ln -s $(pwd)/.zshrc ~/.zshrc
+
+# Create dotfiles symlinks
+ln -s $(pwd)/.gitconfig ~/.gitconfig
