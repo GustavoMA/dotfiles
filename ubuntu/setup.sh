a013a8d51c39ce8070384acd74749a14b4141406
Gustavo Alves <gugamalves@yahoo.com.br>
2021-03-17 16:27:58 -0300

    Install dotnet sdk 3.1

--- ubuntu/setup.sh
+++ ubuntu/setup.sh
@@ -14,3 +14,11 @@ ln -s $(pwd)/.gitconfig ~/.gitconfig
 
 # To play around with redis
 sudo apt -y install redis-tools
+
+# Install dotnet
+# https://docs.microsoft.com/en-us/dotnet/core/install/linux-ubuntu#2004
+wget https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+rm packages-microsoft-prod.deb
+sudo apt-get update
+sudo apt-get install -y apt-transport-https && sudo apt-get update && sudo apt-get install -y dotnet-sdk-3.1
